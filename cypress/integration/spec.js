2a4730bb846ea09c45465322cb09b4b639430a4d
Lisa Manresa <lisa.manresa@roundingwell.com>
2020-09-21 07:19:41 -0700

    Reproduce button enter press issue

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,6 +1,40 @@
 /// <reference types="cypress" />
 describe('page', () => {
-  it('works', () => {
-    cy.visit('https://example.cypress.io')
-  })
+  it('should alert that data has been saved on submit click', () => {
+    const stub = cy.stub()
+    cy.on ('window:alert', stub)
+
+    cy.visit('index.html')
+
+    cy
+      .get('#testName')
+      .type('My Name')
+      .tab()
+      .type('Some details');
+
+      cy
+        .get('#submitBtn')
+        .click()
+        .then(() => {
+          expect(stub).to.be.calledWith('Your data has been saved')
+        });
+  });
+
+  it('should alert that data has been saved on {enter} press of focused submit button', () => {
+    const stub = cy.stub()
+    cy.on ('window:alert', stub)
+
+    cy.visit('index.html')
+
+    cy
+      .get('#testName')
+      .type('My Name')
+      .tab()
+      .type('Some details')
+      .tab()
+      .type('{enter}')
+      .then(() => {
+        expect(stub).to.be.calledWith('Your data has been saved')
+      });
+  });
 })
--- cypress/support/index.js
+++ cypress/support/index.js
@@ -13,6 +13,7 @@
 // https://on.cypress.io/configuration
 // ***********************************************************
 
+require('cypress-plugin-tab');
 // Import commands.js using ES2015 syntax:
 import './commands'
 
